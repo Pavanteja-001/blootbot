93771f102d5f650d096e84313e50d7a5b43d3ff7
Pavanteja Gurajapu <pavantejagurajapu59@gmail.com>
2025-09-18 20:43:05 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,8 @@
+
 import os
 
 # Bind to Render's PORT environment variable
-bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
+bind = f"0.0.0.0:{os.getenv('PORT', '3000')}"  # Changed default to 3000
 workers = 1
 threads = 4
 timeout = 120
